0fcc95b3ce1d6cc9265463e79e54cbda45296b72
Bahit Hamid <bahit.hamid@gmail.com>
2024-03-05 02:32:15 +0800

    Add navigation links to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CrisisListComponent, HeroesListComponent],
+  imports: [RouterOutlet, RouterLink, CrisisListComponent, HeroesListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-router-sample';
